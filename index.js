e8ee5e8e1409bf6dd2c1469d7b5261ffa34dde37
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2024-06-30 14:31:51 +0530

    Updated index.js
    Created 2 player objects and implemented the code for the game class which will orchestrate the interactions between the players and  govern the entire game logic.

---
+++ index.js
@@ -0,0 +1,9 @@
+const Player = require('./player');
+// const Game = require('./game'); -> Game class to be implemented
+
+
+const playerA = new Player('Player A', 50, 5, 10);       
+const playerB = new Player('Player B', 100, 10, 5);
+
+// const game = new Game(playerA, playerB); -> Game class to be implemented
+// game.fight(); -> Game class to be implemented
